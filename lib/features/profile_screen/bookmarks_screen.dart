f6176847de5bc9bfff5b05a9afc0f828093f9630
Chathura Udayanga <138067137+lucifer3618@users.noreply.github.com>
2025-03-16 03:24:36 +0530

    feat: Add fuction to display message if bookmarks screen is empty.

--- lib/features/profile_screen/bookmarks_screen.dart
+++ lib/features/profile_screen/bookmarks_screen.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:readify/features/book_screen/book_screen.dart';
 import 'package:readify/services/database_service.dart';
+import 'package:readify/shared/widgets/empty_data_page.dart';
 import 'package:readify/utils/app_style.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
@@ -22,64 +23,69 @@ class _BookmarksScreenState extends State<BookmarksScreen> {
           style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
         ),
       ),
-      body: Padding(
-          padding: AppStyle.pagePadding.add(const EdgeInsets.only(top: 20)),
-          child: StreamBuilder(
-            stream: DatabaseService().getBookmarkedBooks(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text(snapshot.error.toString()),
-                );
-              } else if (!snapshot.hasData) {
-                return const Center(
-                  child: Text("No data to display!"),
-                );
-              } else {
-                return Skeletonizer(
-                  enabled: snapshot.connectionState == ConnectionState.waiting,
-                  child: GridView.builder(
-                    scrollDirection: Axis.vertical,
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      mainAxisSpacing: 10,
-                      childAspectRatio: 1 / 2,
-                      crossAxisSpacing: 20,
-                    ),
-                    physics: const NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => BookScreen(
-                                  bookId: snapshot.data![index]["id"],
-                                  bookName: snapshot.data![index]["name"],
-                                  author: snapshot.data![index]["author"],
-                                  imagePath: snapshot.data![index]["image_path"],
-                                  currentOwnerId: snapshot.data![index]["currentOwnerId"],
-                                ),
-                              ),
-                            );
-                            DatabaseService().setRecentlyVisitedBook(
-                              snapshot.data![index]["id"],
-                              snapshot.data![index]["name"],
-                              snapshot.data![index]["author"],
-                              snapshot.data![index]["image_path"],
-                              snapshot.data![index]["currentOwnerId"],
-                              snapshot.data![index]["category"],
-                            );
-                          },
-                          child: _bookCard(snapshot.data![index]));
-                    },
+      body: StreamBuilder(
+        stream: DatabaseService().getBookmarkedBooks(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const EmptyDataPage(
+              imgPath: "assets/images/empty-bookmarks.jpg",
+              title: "No Bookmarks Yet?",
+              description:
+                  "Your saved books will appear here. Start exploring and bookmark your favorites for easy access!",
+              wdithFactor: 0.65,
+            );
+          } else {
+            return Padding(
+              padding: AppStyle.pagePadding.add(const EdgeInsets.only(top: 20)),
+              child: Skeletonizer(
+                enabled: snapshot.connectionState == ConnectionState.waiting,
+                child: GridView.builder(
+                  scrollDirection: Axis.vertical,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 1 / 2,
+                    crossAxisSpacing: 20,
                   ),
-                );
-              }
-            },
-          )),
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => BookScreen(
+                                bookId: snapshot.data![index]["id"],
+                                bookName: snapshot.data![index]["name"],
+                                author: snapshot.data![index]["author"],
+                                imagePath: snapshot.data![index]["image_path"],
+                                currentOwnerId: snapshot.data![index]["currentOwnerId"],
+                              ),
+                            ),
+                          );
+                          DatabaseService().setRecentlyVisitedBook(
+                            snapshot.data![index]["id"],
+                            snapshot.data![index]["name"],
+                            snapshot.data![index]["author"],
+                            snapshot.data![index]["image_path"],
+                            snapshot.data![index]["currentOwnerId"],
+                            snapshot.data![index]["category"],
+                          );
+                        },
+                        child: _bookCard(snapshot.data![index]));
+                  },
+                ),
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 
